89deabc419cf74e7fdca2f05312cb2e579d080f6
betofranca <betofe@gmail.com>
2023-01-26 17:44:38 -0600

    Hola

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
-﻿Console.WriteLine("Introduce tu nombre");
+﻿// Ejercicio 1
+
+Console.WriteLine("Introduce tu nombre");
 string nombre = Console.ReadLine();
 Console.WriteLine("Hola, " + nombre);
 
 
+// Ejercicio 2 
+
+DateTime horaactual = DateTime.Now;
+Console.WriteLine("La hora es: " + horaactual.ToString("HH:mm:ss"));
